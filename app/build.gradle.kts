7a48583bd5f4757fb528e174d55a1d7345bbf1a0
John <iplanetcn@gmail.com>
2024-12-31 13:44:13 +0800

    opt gradle config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,4 @@
-val ARCH_ABI by extra(project.properties["ARCH_ABI"] as? String ?: "arm64-v8a")
-val STL_TYPE by extra(project.properties["STL_TYPE"] as? String ?: "c++_shared")
-val SFML_STATIC by extra(project.properties["SFML_STATIC"] as? String ?: "OFF")
+@file:Suppress("UnstableApiUsage")
 
 plugins {
     alias(libs.plugins.com.android.application)
@@ -16,13 +14,15 @@ android {
         targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = libs.versions.versionCode.get().toInt()
         versionName = libs.versions.versionName.get()
+
         ndk {
-            abiFilters.add(ARCH_ABI)
+            abiFilters.add("arm64-v8a")
         }
+
         externalNativeBuild {
             cmake {
-                arguments.add("-DANDROID_STL=${STL_TYPE}")
-                arguments.add("-DSFML_STATIC_LIBRARIES=${SFML_STATIC}")
+                arguments.add("-DANDROID_STL=c++_shared")
+                arguments.add("-DSFML_STATIC_LIBRARIES=OFF")
             }
         }
     }
